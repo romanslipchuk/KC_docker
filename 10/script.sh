2b22421708f3f04224b5eed701b79b7009887ab3
Roman Slipchuk <roman.slipchuk@gmail.com>
2023-05-11 23:38:25 +0200

    Lesson 03 DONE!

---
+++ 10/script.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+name=$1
+len=${#name}
+first_letter=${name::1}
+rest_letters=${name:1:len}
+if [ -z "$name" ]; then
+    echo "Нужно ввести аргумент"
+    exit 1
+else
+  echo "Hello, ${first_letter^}${rest_letters,,} :)"
+fi
